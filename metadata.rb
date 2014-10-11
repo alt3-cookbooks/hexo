87f3e62aa347cff2996f113be23640d814d6b491
bravo-kernel <bravo-kernel@users.noreply.github.com>
2014-10-11 14:38:56 +0200

    Bumps version

--- metadata.rb
+++ metadata.rb
@@ -2,7 +2,7 @@ name             'hexo'
 maintainer       'alt3.io'
 license          'MIT'
 description      'Installs Hexo with optional demo blog.'
-version          '0.1'
+version          '1.0.0'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 
 depends "nodejs"
